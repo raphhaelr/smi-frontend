88f3d11e1f3867344a16c13fd7ab8cca996198cf
Raphael Rodrigues <raphaelrbh7@gmail.com>
2024-01-16 12:51:53 -0300

    fix

--- src/components/Header/Header.tsx
+++
@@ -1,8 +0,0 @@
-export function Header() {
-    return (
-        <section>
-            {/* Include shared UI here e.g. a header or sidebar */}
-            <nav>Header</nav>
-        </section>
-    )
-}
